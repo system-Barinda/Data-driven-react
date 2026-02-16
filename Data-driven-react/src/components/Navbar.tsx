0d5f965cdc1c135062859f754bed798cc427913c
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-16 18:21:45 +0200

    done:

--- Data-driven-react/src/components/Navbar.tsx
+++ Data-driven-react/src/components/Navbar.tsx
@@ -2,7 +2,7 @@ export default function Navbar() {
   return (
     <nav className="h-14 bg-red-500 flex items-center justify-center">
       <span className="text-white text-sm font-medium">
-        🌍 my travel journal.
+         my travel journal.
       </span>
     </nav>
   );
--- Data-driven-react/src/components/TravelCard.tsx
+++ Data-driven-react/src/components/TravelCard.tsx
@@ -1,18 +1,18 @@
-export default function TravelCard({ travel }) {
+export default function TravelCard({ travel }:any) {
   return (
     <div className="flex gap-5 py-6 border-b border-gray-200">
-      {/* Image */}
+     
       <img
         src={travel.image}
         alt={travel.title}
-        className="w-[125px] h-[170px] rounded-lg object-cover"
+        className="w-31.25 h-42.5 rounded-lg object-cover"
       />
 
-      {/* Content */}
+      
       <div className="max-w-md">
-        {/* Location */}
+       
         <div className="flex items-center gap-2 text-xs uppercase tracking-wider">
-          <span className="text-red-500">📍</span>
+          <span className="text-red-500"></span>
           <span className="font-medium text-gray-800">
             {travel.country}
           </span>
@@ -26,17 +26,17 @@ export default function TravelCard({ travel }) {
           </a>
         </div>
 
-        {/* Title */}
+       
         <h2 className="text-2xl font-bold text-gray-900 mt-2">
           {travel.title}
         </h2>
 
-        {/* Date */}
+       
         <p className="text-xs font-bold text-gray-800 mt-2">
           {travel.date}
         </p>
 
-        {/* Description */}
+    
         <p className="text-sm text-gray-700 leading-relaxed mt-2">
           {travel.description}
         </p>
