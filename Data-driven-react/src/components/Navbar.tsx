77681902246dbbdf3892b23a78c2c433b756989d
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-16 18:18:45 +0200

    done

---
+++ Data-driven-react/src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="h-14 bg-red-500 flex items-center justify-center">
+      <span className="text-white text-sm font-medium">
+        🌍 my travel journal.
+      </span>
+    </nav>
+  );
+}
