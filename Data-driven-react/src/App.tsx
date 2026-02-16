fe5cf9ef1736ffa1b1db063def796f537f48db3f
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-16 16:39:37 +0200

    done

--- Data-driven-react/src/App.tsx
+++ Data-driven-react/src/App.tsx
@@ -6,7 +6,7 @@ import TravelList from "./components/TravelList";
 
 export default function App() {
   return (
-    <div>
+    <div className='w-90 h-[100%] bg-green-500 mx-auto'>
       <TravelList />
     </div>
   );
