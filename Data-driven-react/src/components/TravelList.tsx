1c8fb47bfac7e6687207aeebeb5cae53ede533b5
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-16 16:05:59 +0200

    done

---
+++ Data-driven-react/src/components/TravelList.tsx
@@ -0,0 +1,12 @@
+import travels from "../data/travels";
+import TravelCard from "./TravelCard";
+
+export default function TravelList() {
+  return (
+    <div className="travel-list">
+      {travels.map((travel) => (
+        <TravelCard key={travel.id} travel={travel} />
+      ))}
+    </div>
+  );
+}
