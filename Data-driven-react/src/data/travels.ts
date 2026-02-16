ab5f1bc453f1037b3068253ef126d00058b5dd66
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-16 16:03:37 +0200

    chore:done to make file ts of data

---
+++ Data-driven-react/src/data/travels.ts
@@ -0,0 +1,34 @@
+const travels = [
+  {
+    id: 1,
+    country: "Japan",
+    title: "Mount Fuji",
+    date: "12 Jan, 2023 - 24 Jan, 2023",
+    description:
+      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters.",
+    image:
+      "https://images.unsplash.com/photo-1549693578-d683be217e58",
+  },
+  {
+    id: 2,
+    country: "Australia",
+    title: "Sydney Opera House",
+    date: "27 May, 2023",
+    description:
+      "The Sydney Opera House is a multi-venue performing arts centre.",
+    image:
+      "https://images.unsplash.com/photo-1506976785307-8732e854ad03",
+  },
+  {
+    id: 3,
+    country: "Norway",
+    title: "Geirangerfjord",
+    date: "01 Oct, 2024 - 10 Nov, 2024",
+    description:
+      "Geirangerfjord is a fjord in the Sunnmøre region of Norway.",
+    image:
+      "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee",
+  },
+];
+
+export default travels;
