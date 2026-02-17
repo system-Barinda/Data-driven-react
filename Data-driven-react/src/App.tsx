305447f604edeac59334b899b8f64443a802e6df
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-17 19:58:48 +0200

    done

--- Data-driven-react/src/App.tsx
+++ Data-driven-react/src/App.tsx
@@ -1,16 +1,18 @@
 import Navbar from "./components/Navbar";
 import TravelCard from "./components/TravelCard";
 import travels from "./data/travels";
+import UserSlider from "./components/UserSlider";
 
 export default function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar />
       <main className="px-10 py-8 max-w-xl mx-auto">
         {travels.map((travel) => (
           <TravelCard key={travel.id} travel={travel} />
         ))}
-      </main>
+      </main> */}
+      
     </>
   );
 }
