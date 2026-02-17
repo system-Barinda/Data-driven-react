5ee63cdea4e0acf348b7c1bc82933864698741e2
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-17 20:08:37 +0200

    done

--- Data-driven-react/src/App.tsx
+++ Data-driven-react/src/App.tsx
@@ -3,9 +3,9 @@ import TravelCard from "./components/TravelCard";
 import travels from "./data/travels";
 import UserSlider from "./components/UserSlider";
 
-const  App: React.FC = () => {
+const App: React.FC = () => {
   return (
-    
+    <>
       {/* <Navbar />
       <main className="px-10 py-8 max-w-xl mx-auto">
         {travels.map((travel) => (
@@ -13,8 +13,9 @@ const  App: React.FC = () => {
         ))}
       </main> */}
 
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-6">
-      <UserSlider />
-    </div>
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center p-6">
+        <UserSlider />
+      </div>
+    </>
   );
-}
+};
