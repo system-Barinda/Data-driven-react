3b68c2d47deec98c29052032be02590ab3b4781d
system sylvere BARINDA <systembarinda@gmail.com>
2026-02-17 20:03:47 +0200

    done to make it better type of uses

---
+++ Data-driven-react/src/Datatype.type/User.ts
@@ -0,0 +1,14 @@
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+  };
+  address: {
+    city: string;
+    street: string;
+  };
+}
